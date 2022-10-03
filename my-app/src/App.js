fed75715e3e6f4a506e6f5a3cc0ee35fb50a09e2
Mustafa <mustafa_sahinci@hotmail.com>
2022-10-03 14:25:31 +0000

    onclick

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
         <FunctionalGreeting />
-        <FunctionalGreetingWithProps greeting="Nice to meet you" name="Bozkurt" age="40" />  
+        <FunctionalGreetingWithProps greeting="Nice to meet you" name="Bozkurt" age="40" />
+        <StatefulGreeting name="Bozkurt" greeting="This is a stateful greeting" />  
     </div>
   );
 }
